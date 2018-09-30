772a339935fda9bcb64caeee7cfa671b18759d83
tljwvf <tljwvf@mail.missouri.edu>
2018-09-29 23:30:56 -0500

    first commit

---
+++ db_connection.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+var express = require('express');
+var config = require('../../config.js')
+var app = express()
+var path = require('path');
+
+var con = mysql.createConnection({
+	host: config.database.host,
+	user: config.database.user,
+	password: config.database.password
+});
+
+/*app.use('./', function (req, res) {
+	//res.cookie('This is a response form the server via cookies', './', {domain: 'tylerjames.xyz/backend', path './index.html', secure: false});
+	res.send('This is a response from the server')
+})*/
+
+app.get('/test', function(req,res){
+	res.send("You are e piece of degenerate human garbage");
+});
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000,function(){
+  console.log("Started on PORT 3000");
+})
+//con.connect(function(err) {
+//	if (err) throw err;
+//	console.log("Connected!");
+//});
